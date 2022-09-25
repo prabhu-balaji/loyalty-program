657bd803c662e509924fabfa960ec0e7b02ce365
Prabhu <s.prabhu1997@gmail.com>
2022-09-26 00:35:01 +0530

    Adding simplecov

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -11,8 +11,12 @@
 # a separate helper file that requires the additional dependencies and performs
 # the additional setup, and require it from the spec files that actually need
 # it.
+require 'simplecov'
+SimpleCov.start
+
 require 'helper_methods'
 # See https://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
+
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
